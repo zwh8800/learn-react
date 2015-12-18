fd2949530bbcc39052ad6bb1725a8f03ea337b7e
Zhang Weihu <496781108@qq.com>
2015-12-18 08:43:34 +0800

    修改为纯CommonJS

--- src/app/app.js
+++ src/app/app.js
@@ -1,2 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
 var Parent = require('./components/Parent.js');
 ReactDOM.render(<Parent />, document.getElementById('example'));
