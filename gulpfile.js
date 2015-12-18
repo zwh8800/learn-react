69fbd02c10c4fd3651fdbdf8667ee4730e195479
Zhang Weihu <496781108@qq.com>
2015-12-18 09:25:15 +0800

    加入watch & livereload

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var jshint = require('gulp-jshint');
 var connect = require('gulp-connect');
 var sourcemaps = require("gulp-sourcemaps");
 var babel = require("gulp-babel");
+var clean = require('gulp-clean');
 
 var babelify = require("babelify");
 var browserify = require("browserify");
@@ -18,12 +19,18 @@ var src = {
 };
 
 var dest = {
-    base: 'dist',
+    base: 'dist/',
     js: 'dist/js',
-    html: 'dist'
+    html: 'dist/',
+    all: 'dist/**'
 };
 
-gulp.task('jsx', function () {
+gulp.task('clean', function() {
+    gulp.src([dest.all], {read: false})
+        .pipe(clean());
+});
+
+gulp.task('js', function () {
     var b = browserify({ debug: true })
         .transform(babelify.configure({
             presets: ['es2015', 'react']
@@ -44,6 +51,8 @@ gulp.task('html', function () {
         .pipe(gulp.dest(dest.html));
 });
 
+gulp.task('build', ['html', 'js']);
+
 gulp.task('connect', function () {
     connect.server({
         root: '',
@@ -53,9 +62,15 @@ gulp.task('connect', function () {
 });
 
 gulp.task('watch', function () {
-    //gulp.watch([''])
+    gulp.watch(src.js, ['js']);
+    gulp.watch(src.html, ['html']);
+
+    gulp.watch([dest.all]).on('change', function(file) {
+        gulp.src(file.path)
+            .pipe(connect.reload());
+    });
 });
 
 gulp.task('server', ['connect', 'watch']);
 
-gulp.task('default', ['jsx', 'html']);
+gulp.task('default', ['build']);
